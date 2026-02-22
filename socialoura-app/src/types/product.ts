af5fd9a91c3ae0076bb7e348141d88355afb8927
Ilyes <ilyeesx@gmail.com>
2026-02-22 21:04:36 +0100

    amélioration design

--- socialoura-app/src/types/product.ts
+++ socialoura-app/src/types/product.ts
@@ -9,7 +9,7 @@ export interface Product {
   id: string;
   name: string;
   platform: "instagram" | "tiktok" | "youtube" | "facebook";
-  type: "followers" | "likes" | "views";
+  type: "followers" | "likes" | "views" | "subscribers";
   icon: string;
   color: string;
   description: string;
