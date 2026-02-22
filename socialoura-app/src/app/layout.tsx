835820e4aa2d9668a64c5cda6dacf2f7ea79b7c8
Ilyes <ilyeesx@gmail.com>
2026-02-22 03:43:05 +0100

    tracking google ads

--- socialoura-app/src/app/layout.tsx
+++ socialoura-app/src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17893452047"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17964092485"
           strategy="afterInteractive"
         />
         <Script id="google-ads-init" strategy="afterInteractive">
@@ -40,7 +40,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-17893452047');
+            gtag('config', 'AW-17964092485');
           `}
         </Script>
       </head>
